7ac88cff80608d9ac33da34bac8ac378d7298693
ShahabuddinAhmed <shahabuddin.cse.ru@gmail.com>
2017-11-10 01:28:55 +0600

    a flash message is shown

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -80,7 +80,27 @@ body {font-family: "Lato", sans-serif}
             <br>
             <br>
             <br>
-            <div class="col-sm-3"> </div>
+            
+            <div class="col-sm-3">
+                
+            </div>
+            <div class="col-sm-6">
+                @if(session()->has("success"))
+                    <div class="alert alert-success alert-dismissable">
+                        <a href="#" class="close" data-dismiss="alert" aria-label="close">×</a>
+                        <strong>Success!</strong> {{ session()->get("success") }}
+                    </div>
+                @endif
+            </div>
+            
+            <div class="col-sm-3">
+
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-sm-3">
+
+            </div>
             <div class="col-sm-6">
                 <form action="/info" method="POST">
                     {{ csrf_field() }}
